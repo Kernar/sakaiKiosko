8c2af3bf851403a1b71f2e98df9a0d63c9156bc4
Kernar <kernarlarcerus6790@gmail.com>
2024-11-29 17:45:55 -0500

    jsonAPI and interfaces

---
+++ src/app/core/interfaces/product.interface.ts
@@ -0,0 +1,9 @@
+export interface Product {//esto es la interface para usar el json o el API en general
+    id: string;
+    nombre: string;
+    imagen: string;
+    precio: string;
+    marca: string;
+    descripcion: string;
+
+}
--- src/app/core/pages/dashboard/products/products.component.ts
+++ src/app/core/pages/dashboard/products/products.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { AppLayoutModule } from '../../../../layout/app.layout.module';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [],
+  imports: [AppLayoutModule, CommonModule],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
