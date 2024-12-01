a07452a111d2c4d335e4d9e3e5b826234eb44d2d
Kernar <kernarlarcerus6790@gmail.com>
2024-12-01 01:24:09 -0500

    BAKENDproductMarket

--- src/app/core/pages/market/market-products/market-products.component.ts
+++ src/app/core/pages/market/market-products/market-products.component.ts
@@ -5,44 +5,41 @@ import { PaginatorModule } from 'primeng/paginator';
 import { DataViewModule } from 'primeng/dataview';
 import { ButtonModule } from 'primeng/button';
 import { TagModule } from 'primeng/tag';
+import { Product } from '../../../../interfaces/product.interface';
+import { ProductService } from '../../../../services/product.service';
+import { HttpClient } from '@angular/common/http';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-market-products',
   standalone: true,
   imports: [ CommonModule, PaginatorModule, InputTextModule, DataViewModule, ButtonModule, TagModule],
   templateUrl: './market-products.component.html',
-  styleUrl: './market-products.component.css'
+  styleUrl: './market-products.component.css',
+  providers: [
+    HttpClient, ProductService, MessageService
+  ]
   
 })
 export class MarketProductsComponent {
-  productos = [
-    { nombre: 'Producto 1', marca: 'samsung', descripcion: 'Descripción del producto 1', precio: 100, imagenUrl: 'assets/img/sus.jpg', inventoryStatus: 'INSTOCK' },
-    { nombre: 'Producto 2', marca: 'samsung', descripcion: 'Descripción del producto 2', precio: 150, imagenUrl: 'assets/img/sus.jpg', inventoryStatus: 'OUTOFSTOCK' },
-    { nombre: 'Producto 3', marca: 'samsung', descripcion: 'Descripción del producto 3', precio: 200, imagenUrl: 'assets/img/sus.jpg', inventoryStatus: 'INSTOCK' }
-  ]; 
-  
-  
-  searchTerm: string = '';
 
-  constructor() { }
+  productos: Product[] = [];  // Aquí almacenaremos los productos
+
+  constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
-    console.log(this.productos);
-   }
-
-  onSearchChange() {
-    // Lógica para filtrar los productos según el término de búsqueda
-    console.log(this.searchTerm);
+    this.productService.getProducts().subscribe(data => {
+      this.productos = data.map(product => {
+        // Agregar dinámicamente inventoryStatus
+        product['inventoryStatus'] = product.cantidad > 0 ? 'IN STOCK' : 'OUTOFSTOCK';
+        return product;
+      });
+    });
   }
 
-  // Asegura que 'getSeverity' retorna un valor de tipo "success" | "secondary" | "info" | "warning" | "danger" | "contrast"
-  getSeverity(item: any): "success" | "secondary" | "info" | "warning" | "danger" | "contrast" {
-    if (item.inventoryStatus === 'INSTOCK') {
-      return 'success';
-    } else if (item.inventoryStatus === 'OUTOFSTOCK') {
-      return 'danger';
-    }
-    return 'info';  // Si no está ni en "INSTOCK" ni en "OUTOFSTOCK", por defecto será 'info'
+  getSeverity(product: Product): "success" | "danger" {
+    // Mapear 'IN STOCK' a 'success' y 'OUTOFSTOCK' a 'danger'
+    return product.inventoryStatus === 'IN STOCK' ? 'success' : 'danger';
   }
   
   
