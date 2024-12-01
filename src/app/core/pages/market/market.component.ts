dfb8d65395c8d3860597fb10c49d8712abf7b9b1
Kernar <kernarlarcerus6790@gmail.com>
2024-12-01 17:57:53 -0500

    data json actual

--- src/app/core/pages/market/market.component.ts
+++ src/app/core/pages/market/market.component.ts
@@ -4,13 +4,18 @@ import { ButtonModule } from 'primeng/button';
 import { MenubarModule } from 'primeng/menubar';
 import { MarketProductsComponent } from './market-products/market-products.component';
 import { MarketMenubarComponent } from './market-menubar/market-menubar.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-market',
   standalone: true,
-  imports: [CommonModule, MarketProductsComponent, MarketMenubarComponent],
+  imports: [CommonModule, MarketProductsComponent, MarketMenubarComponent, HttpClientModule],
   templateUrl: './market.component.html',
-  styleUrl: './market.component.css'
+  styleUrl: './market.component.css',
+  providers: [
+    HttpClient,  MessageService
+  ]
 })
 export class MarketComponent {
 
