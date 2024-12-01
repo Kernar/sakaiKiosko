77aeb8cb183708fa244d696f72aa45f29f9be81d
Kernar <kernarlarcerus6790@gmail.com>
2024-12-01 12:05:37 -0500

    serviceUser add

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../../interfaces/user.inteface'; // Asegúrate de importar la interfaz de usuario
+import { environment } from '../../../enviroments/enviroment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  private Url: string = environment.baseUrl;
+
+  // Métodos CRUD para usuarios
+
+  // Obtener todos los usuarios
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(`${this.Url}/users`);
+  }
+
+  // Agregar un nuevo usuario
+  addUser(user: User): Observable<User> {
+    return this.http.post<User>(`${this.Url}/users`, user);
+  }
+
+  // Actualizar un usuario existente
+  updateUser(user: User): Observable<User> {
+    return this.http.patch<User>(`${this.Url}/users/${user.id}`, user);
+  }
+
+  // Eliminar un usuario
+  deleteUser(id: number): Observable<User> {
+    return this.http.delete<User>(`${this.Url}/users/${id}`);
+  }
 }
