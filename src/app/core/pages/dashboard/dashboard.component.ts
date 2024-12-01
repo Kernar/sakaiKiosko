d5b854a8858dd934ecfc4386707812e286c7837c
Kernar <kernarlarcerus6790@gmail.com>
2024-12-01 09:29:47 -0500

    footer add

--- src/app/core/pages/dashboard/dashboard.component.ts
+++ src/app/core/pages/dashboard/dashboard.component.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { SharedModule } from 'primeng/api';
+import { ProductsComponent } from './products/products.component';
+import { AppLayoutModule } from '../../../layout/app.layout.module';
+import { LayoutService } from '../../../layout/service/app.layout.service';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [SharedModule],
+  imports: [SharedModule, ProductsComponent, AppLayoutModule, FooterComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
 
+ 
 }
